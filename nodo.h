768afa367b7611b41a03669f786b08b828c30374
Luca <luca.polese@outlook.com>
2020-10-26 10:14:51 +0100

    First version

---
+++ nodo.h
@@ -0,0 +1,15 @@
+#ifndef NODO_H
+#define NODO_H
+#include <iostream>
+
+class Nodo {
+private:
+    //Costruttore del nodo a 0,1,2,3 parametri, in tutti i casi i parametri passati per valore, verranno assegnati
+    //richiamando la costruzione di copia o assegnando l'indirizzo al puntatore specifico
+    Nodo(char c='*', Nodo* s=0, Nodo* d=0) : info(c), sx(s), dx(d) {}
+    char info;
+    Nodo* sx;
+    Nodo* dx;
+};
+
+#endif //TREE_NODO_H
